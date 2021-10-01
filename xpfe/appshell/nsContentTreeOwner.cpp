33b1157cefdfe64e19379f6e78d9ef0b1a3c37fe
Raine Makelainen <raine.makelainen@jolla.com>
2021-10-01 13:52:17 +0300

    [PATCH] [sailfishos][gecko] Revert "Bug 1494175 - Remove unimplemented nsIWebBrowserChrome methods. r=qdot"
    This partially reverts commit 578ac09f67274b520071a3ef0052405cde0ef9f0.
    
    Sailfish OS embedding requires destroyBrowserWindow to handle
    window.close (OnWindowCloseRequested).
    
    Signed-off-by: Raine Makelainen <raine.makelainen@jolla.com>

--- xpfe/appshell/nsContentTreeOwner.cpp
+++ xpfe/appshell/nsContentTreeOwner.cpp
@@ -377,6 +377,11 @@ NS_IMETHODIMP nsContentTreeOwner::IsWindowModal(bool* _retval) {
   return NS_OK;
 }
 
+NS_IMETHODIMP nsContentTreeOwner::DestroyBrowserWindow() {
+   NS_ERROR("Haven't Implemented this yet");
+   return NS_ERROR_FAILURE;
+}
+
 //*****************************************************************************
 // nsContentTreeOwner::nsIBaseWindow
 //*****************************************************************************
