8488b972d602e2744ef6a67b1bbaf0e4097bbcfe
Matti Lehtimäki <matti.lehtimaki@jolla.com>
2025-02-24 17:40:16 +0200

    Revert "[PATCH] [sailfishos][gecko] Create EmbedLiteCompositorBridgeParent in CompositorManagerParent (part 2). JB#50505"
    This reverts commit 120b5b368d14dc8e65b56066ba58631cb8f55bb7.

--- gfx/layers/ipc/CompositorManagerParent.cpp
+++ gfx/layers/ipc/CompositorManagerParent.cpp
@@ -18,11 +18,6 @@
 #include "gfxPlatform.h"
 #include "VsyncSource.h"
 
-#if defined(MOZ_EMBEDLITE)
-#include "mozilla/embedlite/nsWindow.h"
-#include "mozilla/embedlite/EmbedLiteCompositorBridgeParent.h"
-#endif
-
 namespace mozilla {
 namespace layers {
 
@@ -112,17 +107,9 @@ CompositorManagerParent::CreateSameProcessWidgetCompositorBridge(
   TimeDuration vsyncRate =
       gfxPlatform::GetPlatform()->GetGlobalVsyncDispatcher()->GetVsyncRate();
 
-#ifdef MOZ_EMBEDLITE
-  // auto window = static_cast<mozilla::embedlite::nsWindow *>(aWidget->RealWidget());
-  RefPtr<CompositorBridgeParent> bridge =
-          new mozilla::embedlite::EmbedLiteCompositorBridgeParent(1, sInstance, aScale, vsyncRate, aOptions,
-                                                                  aUseExternalSurfaceSize, aSurfaceSize,
-                                                                  aInnerWindowId);
-#else
   RefPtr<CompositorBridgeParent> bridge = new CompositorBridgeParent(
       sInstance, aScale, vsyncRate, aOptions, aUseExternalSurfaceSize,
       aSurfaceSize, aInnerWindowId);
-#endif
 
   sInstance->mPendingCompositorBridges.AppendElement(bridge);
   return bridge.forget();
